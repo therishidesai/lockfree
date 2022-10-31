60e8b18d338816ceb8fad31d8390d8728170c091
Rishi Desai <desai.rishi1@gmail.com>
2022-10-30 17:24:09 -0700

    simple lock free spsc_queue working

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,5 +1,28 @@
 use spsc_queue;
 
+use std::thread;
+
 fn main() {
-    spsc_queue::hello();
+    let (tx, rx) = spsc_queue::new_queue(500);
+
+    thread::spawn(move|| {
+        for i in 0..100000 {
+            loop {
+                match tx.try_push(i) {
+                    None => break,
+                    Some(_) => {},
+                }
+            }
+            //println!("Wrote {}", i);
+        }
+    });
+
+    for i in 0..100000 {
+        loop {
+            match rx.try_pop() {
+                None => {}
+                Some(v) => { assert!(v == i, "v: {} != i: {}", v, i); break; }
+            }
+        }
+    }
 }
